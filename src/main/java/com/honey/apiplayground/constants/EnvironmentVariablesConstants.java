50cd117df27b4da878ff5b0d826253d2bc43777b
Michael Toninger Polidoro <michaeltp.2012@hotmail.com>
2024-07-17 19:37:54 -0300

    Added EnvironmentVariablesConstants

---
+++ src/main/java/com/honey/apiplayground/constants/EnvironmentVariablesConstants.java
@@ -0,0 +1,9 @@
+package com.honey.apiplayground.constants;
+
+public final class EnvironmentVariablesConstants {
+
+    private EnvironmentVariablesConstants() {}
+
+    public static final String CACHE_REQUEST_BODIES = "cache_request_bodies";
+    public static final String PARALLEL_SCENARIOS_COUNT = "parallel_scenarios_count";
+}
