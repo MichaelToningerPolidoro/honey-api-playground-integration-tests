1f64da78bdb4b126ec80a4a25771263b5e20d670
Michael Toninger Polidoro <michaeltp.2012@hotmail.com>
2025-11-13 18:28:01 -0300

    Added standard configuration before tests start

--- src/main/java/com/honey/apiplayground/hooks/HookBefore.java
+++ src/main/java/com/honey/apiplayground/hooks/HookBefore.java
@@ -3,8 +3,15 @@ package com.honey.apiplayground.hooks;
 import com.honey.apiplayground.context.ScenarioContextStorage;
 import com.honey.apiplayground.managers.RequestManager;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
+import io.restassured.RestAssured;
 
 public final class HookBefore {
+
+    @BeforeAll
+    public static void beforeAllConfig() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
 
     @Before
     public void before() {
