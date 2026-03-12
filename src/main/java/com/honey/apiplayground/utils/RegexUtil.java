d8ff9bca6238b55b776b064ec79f969ad4d0e132
Michael Toninger Polidoro <michaeltp.2012@hotmail.com>
2026-03-12 18:29:53 -0300

    Adjusted regex that split text between curly braces

--- src/main/java/com/honey/apiplayground/utils/RegexUtil.java
+++ src/main/java/com/honey/apiplayground/utils/RegexUtil.java
@@ -2,6 +2,7 @@ package com.honey.apiplayground.utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,10 +14,12 @@ public final class RegexUtil {
         final List<String> foundValues = new ArrayList<>();
         final Matcher matcher = pattern.matcher(text);
 
-        // TODO use group count
-//        int a = matcher.groupCount();
-        while (matcher.find()) {
-            foundValues.add(matcher.group());
+        if (!matcher.matches()) {
+            throw new NoSuchElementException("Couldn't find any matches!");
+        }
+
+        for (int i = 1; i <= matcher.groupCount(); i++) {
+            foundValues.add(matcher.group(i));
         }
 
         return foundValues;
